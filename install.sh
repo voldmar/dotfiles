3640a00a3edde04fdbb3e3c2913f0f58e0226b0a
Vladimir Epifanov <voldmar@voldmar.ru>
2012-03-22 15:26:43 +0400

    Installation file

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILES="colordiffrc environment gitconfig gitignore tmux.conf zshrc"
+
+cd $HOME
+
+git clone git@github.com:voldmar/dotfiles etc
+
+for file in $FILES
+do
+    ln -s etc/$file .$file
+done
+
