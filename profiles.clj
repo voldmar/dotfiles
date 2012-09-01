042d272c97c673944a7a000628786d1b8fc2172d
Vladimir Epifanov <voldmar@voldmar.com>
2012-09-01 20:11:48 +0400

    New .lein/profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,21 @@
+{:user {:plugins [[lein-marginalia "0.7.0"]
+                  [lein-swank "1.4.4"]
+                  [lein-cljsbuild "0.2.5"]
+                  [lein-repls "1.9.5"]
+                  ]
+:dependencies  {clj-stacktrace "0.2.4"} :injections  [(let  [orig  (ns-resolve
+                                                                      (doto
+                                                                        'clojure.stacktrace
+                                                                        require)
+                                                                        'print-cause-trace)
+                                                                      new
+                                                                      (ns-resolve
+                                                                        (doto
+                                                                          'clj-stacktrace.repl
+                                                                          require)
+                                                                        'pst)]
+                                                                      (alter-var-root
+                                                                        orig
+                                                                        (constantly
+                                                                          @new)))] } 
+ }
