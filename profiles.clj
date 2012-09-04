c6345454ad3234447b77e4548605dec2af154e22
Vladimir Epifanov <voldmar@voldmar.com>
2012-09-04 16:36:29 +0400

    Reformated profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,21 +1,25 @@
-{:user {:plugins [[lein-marginalia "0.7.0"]
-                  [lein-swank "1.4.4"]
-                  [lein-cljsbuild "0.2.5"]
-                  [lein-repls "1.9.5"]
-                  ]
-:dependencies  {clj-stacktrace "0.2.4"} :injections  [(let  [orig  (ns-resolve
-                                                                      (doto
-                                                                        'clojure.stacktrace
-                                                                        require)
-                                                                        'print-cause-trace)
-                                                                      new
-                                                                      (ns-resolve
-                                                                        (doto
-                                                                          'clj-stacktrace.repl
-                                                                          require)
-                                                                        'pst)]
-                                                                      (alter-var-root
-                                                                        orig
-                                                                        (constantly
-                                                                          @new)))] } 
- }
+{:user {
+  :plugins [[lein-marginalia "0.7.0"]
+            [lein-swank "1.4.4"]
+            [lein-cljsbuild "0.2.5"]
+            [lein-repls "1.9.5"]
+            [lein-newnew "0.3.4"]
+            [clj-stacktrace "0.2.4"]
+              ]
+  :dependencies  {clj-stacktrace "0.2.4"}
+  :injections  [
+      (let  [orig  (ns-resolve
+                     (doto
+                       'clojure.stacktrace
+                       require)
+                     'print-cause-trace)
+             new
+             (ns-resolve
+               (doto
+                 'clj-stacktrace.repl
+                 require)
+               'pst)]
+        (alter-var-root
+          orig
+          (constantly
+            @new)))]}}
