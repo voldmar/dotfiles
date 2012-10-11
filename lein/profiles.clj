2bd5ccea2bbd8761d2bc5341b23f796503eb68c9
Vladimir Epifanov <voldmar@voldmar.com>
2012-10-11 12:57:28 +0400

    New versions of Lein deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,13 @@
 {:user {
   :plugins [[lein-marginalia "0.7.0"]
             [lein-swank "1.4.4"]
-            [lein-cljsbuild "0.2.5"]
+            [lein-cljsbuild "0.2.7"]
             [lein-repls "1.9.5"]
             [lein-newnew "0.3.4"]
             [clj-stacktrace "0.2.4"]
-              ]
+            [lein-ring "0.7.5"]
+            [lein-pprint "1.1.1"]
+            [ring-mock "0.1.3"]]
   :dependencies  {clj-stacktrace "0.2.4"}
   :injections  [
       (let  [orig  (ns-resolve
