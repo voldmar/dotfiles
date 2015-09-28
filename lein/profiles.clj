f0fa78ff514829d06dda86cb48041ce386ad3049
Vladimir Epifanov <voldmar@voldmar.com>
2015-09-28 15:29:51 +0300

    Leiningen profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,9 @@
-{:user {}}
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-ancient "0.6.7"]
+                  [lein-cljfmt "0.3.0"]
+                  [lein-gorilla "0.3.4"]
+                  [lein-oneoff "0.3.2"]
+                  [lein-plz "0.3.3"]
+                  [lein-try "0.4.3"]]
+        :ultra  {:color-scheme :solarized_dark}}}
